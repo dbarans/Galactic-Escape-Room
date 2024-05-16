454cb5a4f9bf66e37e154e0a9639417f9954c9ef
Dominik Barański <baranskidominik109@gmail.com>
2024-05-16 14:09:48 +0200

    update

--- Assets/Player/ActionRay.cs
+++ Assets/Player/ActionRay.cs
@@ -23,6 +23,15 @@ public class ActionRay : MonoBehaviour
     }
     private void CastRay()
     {
-        
+       
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+    
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Trafiony obiekt: " + hit.collider.gameObject.name);
+
+        }
     }
 }
