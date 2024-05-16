58cc1ae732bf7c3bef29434751dfbbd2cdc0b597
Jakub Białasik <kuba.bialasik.2002@gmail.com>
2024-05-16 14:23:45 +0200

    breakable window

--- Assets/Breakable.cs
+++ Assets/Breakable.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 public class Breakable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject intactwindow;
+    [SerializeField] GameObject brokenwindow;
+
+    BoxCollider bc;
+
+    private void Awake()
     {
-        
+        intactwindow.SetActive(true);
+        brokenwindow.SetActive(false);
+
+        bc = GetComponent<BoxCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log("Collision detected with " + collision.gameObject.name); // Debugging line
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb != null && rb.velocity.magnitude > 2) // U¿yj velocity z Rigidbody
+        {
+            Break();
+        }
+    }
+
+    private void Break()
+    {
+        Debug.Log("Window is breaking!"); // Debugging line
+        intactwindow.SetActive(false);
+        Debug.Log("Intact window deactivated."); // Debugging line
+        brokenwindow.SetActive(true);
+        Debug.Log("Broken window activated."); // Debugging line
+
+        bc.enabled = false;
     }
 }
