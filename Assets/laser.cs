c7c2503c9da58cae2ce272cf23b24b649e0a45f5
Jakub Białasik <kuba.bialasik.2002@gmail.com>
2024-05-23 14:40:23 +0200

    drugie pomieszczenie

--- Assets/laser.cs
+++ Assets/laser.cs
@@ -1,55 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-public class laser : MonoBehaviour
+public class LaserCollision : MonoBehaviour
 {
-    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Transform spawnPoint; // Punkt spawnu gracza
 
-    [SerializeField] private float laserDistance = 8f;
-
-    [SerializeField] private LayerMask ignoreMask;
-
-    [SerializeField] private UnityEvent OnHitTarget;
-
-    private RaycastHit rayHit;
-    private Ray ray;
-
-
-    private void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        lineRenderer.positionCount = 2;
-    }
-
-    private void Update()
-
-    {
-        ray = new(transform.position, transform.forward);
-
-        if(Physics.Raycast(ray, out rayHit, laserDistance, ~ignoreMask))
+        if (other.CompareTag("Player"))
         {
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, rayHit.point);
-            if(rayHit.collider.TryGetComponent(out TargetJoint2D target))
-            {
-                target.Hit();
-                OnHitTarget?.Invoke();
-            }
+            other.transform.position = spawnPoint.position; // Teleportuj gracza do punktu spawnu
+            other.transform.rotation = spawnPoint.rotation; // Ustaw rotacjê gracza na rotacjê punktu spawnu
         }
-        else
-        {
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, transform.position + transform.forward * laserDistance);
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(ray.origin, ray.direction * laserDistance);
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(rayHit.point, 0.23f);
     }
 }
